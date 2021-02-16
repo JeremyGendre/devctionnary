ebc483b871e086c8b8666846c114b6c71eb24a84
Anass DAEF <anassdaef@gmail.com>
2021-02-16 14:10:54 +0100

    build(ajout des dependencies): ajout d'angular material et flex layout
    #1

--- web/devctionnary-front/src/app/app.module.ts
+++ web/devctionnary-front/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    FlexLayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
