d88e3caf64fe3f3ac9a8b98ae92c061dfa85ccea
Jérémy Gendre <gendrejeremy@yahoo.fr>
2021-02-17 11:32:54 +0100

    feat(controller): add BaseAbstractController which provide basic return methods

---
+++ api/src/Controller/BaseAbstractController.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class BaseAbstractController extends AbstractController
+{
+    /**
+     * @param string $message
+     * @return JsonResponse
+     */
+    public function errorJsonResponse(string $message = 'Une erreur est survenue'){
+        return new JsonResponse([
+            'success' => false,
+            "message" => $message
+        ]);
+    }
+
+    /**
+     * @param $data
+     * @return JsonResponse
+     */
+    protected function successJsonResponse($data = null){
+        return new JsonResponse([
+            'success' => true,
+            'data' => $data
+        ]);
+    }
+}
