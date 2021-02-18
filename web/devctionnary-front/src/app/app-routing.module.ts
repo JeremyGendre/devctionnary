d7c5c499fcd57d652cd26c78c13ad1aa31a9ec4e
Anass DAEF <anassdaef@gmail.com>
2021-02-18 10:13:29 +0100

    feat(navigation): navigation bar

--- web/devctionnary-front/src/app/app-routing.module.ts
+++ web/devctionnary-front/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AllSnippetComponent } from './all-snippet/all-snippet.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', component: AllSnippetComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- web/devctionnary-front/src/app/app.module.ts
+++ web/devctionnary-front/src/app/app.module.ts
@@ -1,22 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {MatGridListModule} from '@angular/material/grid-list';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatToolbarModule} from '@angular/material/toolbar';
+import  {MatIconModule } from '@angular/material/icon';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
+import { FooterComponent } from './footer/footer.component';
+import { AllSnippetComponent } from './all-snippet/all-snippet.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    AllSnippetComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NoopAnimationsModule,
     FlexLayoutModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatIconModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
