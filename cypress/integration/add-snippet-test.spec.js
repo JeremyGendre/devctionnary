dcf446ffbbf1431cceeb7f1597d95a035c98dbb2
reddu25 <43440036+reddu25@users.noreply.github.com>
2021-03-09 15:30:16 +0100

    Merge branch 'master' into develop

--- cypress/integration/add-snippet-test.spec.js
+++ cypress/integration/add-snippet-test.spec.js
@@ -1,21 +1,42 @@
 describe("User", () => {
   it("should add snippet", () => {
     cy.fixture('user.json').then((user) => {
+      // Register.
+      cy.visit('/register');
+      cy.get('input[formcontrolname="username"]').type(user.username);
+      cy.get('input[formcontrolname="password"]').type(user.password);
+      cy.get('input[formcontrolname="firstName"]').type(user.firstName);
+      cy.get('input[formcontrolname="lastName"]').type(user.lastName);
+      cy.get('input[formcontrolname="email"]').type(user.email);
+      cy.get('form').submit();
+
+      cy.wait(3000);
+
+      // Login
       cy.visit('/login');
       cy.get('input[formcontrolname="username"]').type(user.username);
       cy.get('input[formcontrolname="password"]').type(user.password);
       cy.get('form').submit();
+
+      cy.wait(3000);
       cy.contains('Profil').should('be.visible');
-  
+
       cy.visit('/add-snipet');
       cy.get('input[formcontrolname="title"]').type('My snippet');
       cy.get('input[formcontrolname="description"]').type('My snippet description');
       cy.get('input[formcontrolname="content"]').type('<p><strong>My content</strong></p>');
       cy.get('form').submit();
   
-      cy.wait(2000);
+      cy.wait(3000);
       cy.visit('/');
       cy.contains('My snippet').should('be.visible');
+
+      // Remove
+      cy.visit('/profile');
+      cy.get('button[color="warn"]').click();
+      cy.get('button[mat-dialog-close][color="warn"]').click();
+      cy.wait(3000);
+      cy.contains('Connexion').should('be.visible');
     });
   });
 });
--- cypress/integration/edit-profile-test.spec.js
+++ cypress/integration/edit-profile-test.spec.js
@@ -1,13 +1,29 @@
 describe("User", () => {
   it("should edit user profile", () => {
     cy.fixture('user.json').then((user) => {
+      // Register.
+      cy.visit('/register');
+      cy.get('input[formcontrolname="username"]').type(user.username);
+      cy.get('input[formcontrolname="password"]').type(user.password);
+      cy.get('input[formcontrolname="firstName"]').type(user.firstName);
+      cy.get('input[formcontrolname="lastName"]').type(user.lastName);
+      cy.get('input[formcontrolname="email"]').type(user.email);
+      cy.get('form').submit();
+
+      cy.wait(3000);
+
+      // Login
       cy.visit('/login');
       cy.get('input[formcontrolname="username"]').type(user.username);
       cy.get('input[formcontrolname="password"]').type(user.password);
       cy.get('form').submit();
+
+      cy.wait(3000);
       cy.contains('Profil').should('be.visible');
+
+      // Edit profile
       cy.visit('/modify-profile');
-      cy.wait(2000);
+      cy.wait(3000);
   
       cy.get('textarea[formcontrolname="biography"').clear();
       cy.get('textarea[formcontrolname="biography"').type('I like trains');
@@ -16,6 +32,13 @@ describe("User", () => {
   
       cy.wait(3000);
       cy.contains('I like trains').should('be.visible');
+
+      // Remove
+      cy.visit('/profile');
+      cy.get('button[color="warn"]').click();
+      cy.get('button[mat-dialog-close][color="warn"]').click();
+      cy.wait(3000);
+      cy.contains('Connexion').should('be.visible');
     });
   });
 });
