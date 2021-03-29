82f014c2183d6d063d2150a9f1880a2184fea600
Jérémy Gendre <gendrejeremy@yahoo.fr>
2021-03-29 14:52:04 +0200

    fix(lint): js lint

--- web/devctionnary-front/src/app/details-snippet/details-snippet.component.spec.ts
+++ web/devctionnary-front/src/app/details-snippet/details-snippet.component.spec.ts
@@ -1,8 +1,5 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { DetailsSnippetComponent } from './details-snippet.component';
